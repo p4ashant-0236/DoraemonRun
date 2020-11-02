fb514da4422ef28baa6ee2e9c9f9611d3fbb5444
Prashant Ramani <prashantramani7689@gmail.com>
2020-11-02 20:57:05 +0530

    2020-11-02

---
+++ Assets/Scripts/Collectible/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour,Collectible
+{
+    [SerializeField]
+    private AnimationCurve curve;
+
+    public void Collect()
+    {
+        Destroy(transform.gameObject);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y+curve.Evaluate(Time.time), transform.position.z);    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Collect();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float dir = 1;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private bool Grounded = true;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float jumpSpeed;
+
+
+    void Start()
+    {
+        rigidBody = transform.GetComponent<Rigidbody2D>();        
+        animator = transform.GetComponent<Animator>();
+        animator.SetBool("Grounded",true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * dir * speed, transform.position.y, transform.position.z);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+            Grounded = false;
+            animator.SetBool("Grounded",false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && Grounded==false)
+        {
+            animator.SetBool("Grounded",true);
+            Grounded = true;
+        }else if (collision.gameObject.CompareTag("SideWall"))
+        {
+            dir *= -1;
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
